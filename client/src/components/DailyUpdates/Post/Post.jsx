ffcf6753d71022f9ea3253023c01b68b50838a80
Hasibul-Islam-Shanto <mdhasibulislamshanto007@gmail.com>
2022-01-04 12:12:41 +0600

    added type

--- client/src/components/DailyUpdates/Post/Post.jsx
+++ client/src/components/DailyUpdates/Post/Post.jsx
@@ -1,12 +1,22 @@
 import axios from "axios";
 import React, { useState} from "react";
+import Select from "react-select";
 import "./Post.css";
 import jwt_decode from "jwt-decode";
+
+
+const options = [
+  { value: 'Educational', label: 'Educational' },
+  { value: 'Departmental', label: 'Departmental' },
+  { value: 'Official', label: 'Official' },
+  { value: 'Political', label: 'Political' },
+];
+
 const Post = () => {
   const [title, setTitle] = useState("");
   const [description, setdescription] = useState("");
   const [multipleFiles, setMultipleFiles] = useState("");
-
+  const [type, setType] = useState("")
 
   const MultipleFileChange = (e) => {
     setMultipleFiles(e.target.files);
@@ -18,6 +28,7 @@ const Post = () => {
      const formData = new FormData();
      formData.append("title",title);
      formData.append("description", description);
+     formData.append("type", type)
      formData.append("name", decoded.name);
      formData.append("department", decoded.department);
      for (let i = 0; i < multipleFiles.length; i++) {
@@ -58,7 +69,27 @@ const Post = () => {
               className="file"
               onChange={(e) => MultipleFileChange(e)}
             />
+            
+            <Select
+            options= {options}
+            onChange= {(e) => setType(e.target.value)}
+            placeholder="Type"
+            styles={{
+              container: (provided, state) => ({
+                ...provided,
+                width: "200px",
+                marginLeft: "20px",
+                border: "1px soldi gray "
+              }),
+              control: (provided, state) => ({
+                ...provided,
+                border : 0,
+                boxShadow : "none"
+              })
+            }}
+          />
           </div>
+
           <button
             type="button"
             onClick={() => UploadMultipleFiles()}
